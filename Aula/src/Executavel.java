02c59b9bc70f9b2d4284348986607bec66e5deb1
Matheus Luiz <matheus_luiz2011@hotmail.com>
2022-05-25 05:30:20 -0300

    Aula 7

--- Aula/src/Executavel.java
+++ Aula/src/Executavel.java
@@ -1,8 +1,28 @@
 public class Executavel {
     public static void main(String[] args) {
-        ControleRemoto controle = new ControleRemoto();
-        controle.ligar();
-        controle.abrirMenu();
+       
+        Lutador l1 = new Lutador("Pretty Boy", "França", 31 , 1.75, 68.9, "Leve", 11, 2, 1);
+       
 
-    }
+        Lutador l2 = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, "Leve", 14, 2, 3);
+       
+
+       Lutador l3 = new Lutador("Snapshadow", "EUA", 35, 1.65, 80.9, "Médio", 12, 2, 1);
+       
+
+       Lutador l4 = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, "Médio", 13, 0, 2);
+       
+       Lutador l5 = new Lutador("Ufocobol", "Brasil", 37, 1.70, 119.3, "Pesado", 5, 4, 3);
+       
+
+       Lutador l6 = new Lutador("Nerdaard", "EUA", 30, 1.81, 105.7, "Pesado", 12, 2, 4);
+    
+       l1.apresentar();
+       l2.apresentar();
+       l3.apresentar();
+       l4.apresentar();
+       l5.apresentar();
+       l6.apresentar();
+
+    }   
 }
---
+++ Aula/src/Luta.java
@@ -0,0 +1,13 @@
+public class Luta {
+    private String desafiado;
+    private String desafiante;
+    private int rounds;
+    private String aprovada;
+
+    public void marcarLuta(){
+
+    }
+    public void lutar(){
+        
+    }
+}
