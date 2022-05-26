d47d48e9af1eac660b213b1b4cfc5c27dfdaecde
Matheus Luiz <matheus_luiz2011@hotmail.com>
2022-05-26 15:49:04 -0300

    Finalização Aula12

---
+++ POO/Aula12/README.md
@@ -0,0 +1,3 @@
+## Resumo Aula 12
+
+Aula para fixação do conceito de Polimorfismo tipo Override
---
+++ POO/Aula12/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main(String[] args) {
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new Ave();
+    }
+     
+}
