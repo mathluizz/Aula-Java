0dd0041b694d0666be53bdb663f14a3d3dd2e515
Matheus Luiz <matheus_luiz2011@hotmail.com>
2022-08-22 23:26:27 -0300

    Finalização aula 247

--- Exercicios/Set/Set-Exercicio/src/application/Program.java
+++ Exercicios/Set/Set-Exercicio/src/application/Program.java
@@ -1,45 +1,46 @@
 package application;
 
-import model.entities.Course;
-
+import java.text.ParseException;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Program {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws ParseException {
 
         Scanner sc = new Scanner(System.in);
-        Set<Course> set = new HashSet<>();
+
+        Set<Integer> a = new HashSet<>();
+        Set<Integer> b = new HashSet<>();
+        Set<Integer> c = new HashSet<>();
 
         System.out.print("How many students for course A? ");
-        int qntA = sc.nextInt();
-        for(int i=0; i<qntA; i++){
-            System.out.print("Student: ");
-            set.add (new Course(sc.nextInt()));
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int number = sc.nextInt();
+            a.add(number);
         }
-
-
 
         System.out.print("How many students for course B? ");
-        int qntB = sc.nextInt();
-        for(int i=0; i<qntB; i++){
-            sc.nextLine();
-            System.out.print("Student: ");
-            set.add (new Course(sc.nextInt()));
+        n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int number = sc.nextInt();
+            b.add(number);
         }
-
 
         System.out.print("How many students for course C? ");
-        int qntC = sc.nextInt();
-        for(int i=0; i<qntC; i++){
-            sc.nextLine();
-            System.out.print("Student: ");
-            set.add (new Course(sc.nextInt()));
+        n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int number = sc.nextInt();
+            c.add(number);
         }
 
+        Set<Integer> total = new HashSet<>(a);
+        total.addAll(b);
+        total.addAll(c);
 
-        System.out.println("Total students: " + set.size());
+        System.out.println("Total students: " + total.size());
 
         sc.close();
     }
