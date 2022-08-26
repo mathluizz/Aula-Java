1fbeb69c823995dd673b7f1302ae8afbcbb3a3b4
Matheus Luiz <matheus_luiz2011@hotmail.com>
2022-08-26 15:15:49 -0300

    first commit

---
+++ JDBC/jdbc4/src/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DbIntegrityException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Connection conn = null;
+		PreparedStatement st = null;
+
+		try {
+			conn = DB.getConnection();
+
+			st = conn.prepareStatement(
+				"DELETE FROM department "
+				+ "WHERE "
+				+ "Id = ?");
+
+			st.setInt(1, 5);
+
+			int rowsAffect = st.executeUpdate();
+
+			System.out.println("Done! Rows affected :" + rowsAffect);
+			
+		}catch(SQLException e){
+			throw new DbIntegrityException(e.getMessage());
+		}finally{
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
+		
+	}
+}
